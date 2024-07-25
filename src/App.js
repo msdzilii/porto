c5b035d73793dee77808f15f8a19d6e5459646c0
msdzilii <83410300+msdzilii@users.noreply.github.com>
2024-07-25 20:33:39 +0700

    ke1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from './logo.svg'
+import { useState } from 'react'
+
+import { Tabs, TabList, TabPanels, Tab, TabPanel } from '@chakra-ui/react'
+import { Container } from '@chakra-ui/react'
+
+import './App.css'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <Tabs variant='soft-rounded' colorScheme='green'>
+        <TabList>
+          <Tab>Tab 1</Tab>
+          <Tab>Tab 2</Tab>
+        </TabList>
+        <TabPanels>
+          <TabPanel>
+            <Container>
+              There are many benefits to a joint design and development system.
+              Not only does it bring benefits to the design team, but it also
+              brings benefits to engineering teams. It makes sure that our
+              experiences have a consistent look and feel, not just in our
+              design specs, but in production
+            </Container>
+          </TabPanel>
+          <TabPanel>
+            <p>two!</p>
+          </TabPanel>
+        </TabPanels>
+      </Tabs>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
