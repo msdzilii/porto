13e5a8c224eca6fe750de866010e315375387492
msdzilii <83410300+msdzilii@users.noreply.github.com>
2024-07-28 13:31:17 +0700

    up2

--- src/App.js
+++ src/App.js
@@ -1,36 +1,31 @@
-import logo from './logo.svg'
-import { useState } from 'react'
+import logo from "./logo.svg";
+import { useState } from "react";
 
-import { Tabs, TabList, TabPanels, Tab, TabPanel } from '@chakra-ui/react'
-import { Container } from '@chakra-ui/react'
+import { Popover, PopoverButton, PopoverPanel } from "@headlessui/react";
 
-import './App.css'
+import "./App.css";
 
 function App() {
   return (
-    <div className='App'>
-      <Tabs variant='soft-rounded' colorScheme='green'>
-        <TabList>
-          <Tab>Tab 1</Tab>
-          <Tab>Tab 2</Tab>
-        </TabList>
-        <TabPanels>
-          <TabPanel>
-            <Container>
-              There are many benefits to a joint design and development system.
-              Not only does it bring benefits to the design team, but it also
-              brings benefits to engineering teams. It makes sure that our
-              experiences have a consistent look and feel, not just in our
-              design specs, but in production
-            </Container>
-          </TabPanel>
-          <TabPanel>
-            <p>two!</p>
-          </TabPanel>
-        </TabPanels>
-      </Tabs>
+    <div className="App">
+      <PopoverGroup>
+        <Popover>
+          <PopoverButton>Product</PopoverButton>
+          <PopoverPanel>{/* ... */}</PopoverPanel>
+        </Popover>
+
+        <Popover>
+          <PopoverButton>Solutions</PopoverButton>
+          <PopoverPanel>{/* ... */}</PopoverPanel>
+        </Popover>
+
+        <Popover>
+          <PopoverButton>Pricing</PopoverButton>
+          <PopoverPanel>{/* ... */}</PopoverPanel>
+        </Popover>
+      </PopoverGroup>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
