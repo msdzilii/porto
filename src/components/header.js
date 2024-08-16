c82f168026a87be369be21c91854e678e58fe727
msdzilii <83410300+msdzilii@users.noreply.github.com>
2024-08-16 17:15:46 +0700

    ubah

--- src/components/header.js
+++ src/components/header.js
@@ -4,24 +4,24 @@ import { Popover, PopoverButton, PopoverPanel } from '@headlessui/react';
 
 const Header = () => {
   return (
-    <header className=" bg-blue1 shadow">
+    <header className=" bg-white shadow">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex justify-end items-center py-6">
           <div className="flex items-center">
             <nav className="hidden md:flex space-x-10 ml-10">
-              <a href="#aboutme" className="text-base font-medium text-white hover:text-gray-900">
+              <a href="#aboutme" className="text-base font-medium text-blue1 hover:text-gray-900">
                 Tentang Saya
               </a>
-              <a href="#experience" className="text-base font-medium text-white hover:text-gray-900">
+              <a href="#experience" className="text-base font-medium text-blue1 hover:text-gray-900">
                 Pengalaman 
               </a>
-              <a href="#skill" className="text-base font-medium text-white hover:text-gray-900">
+              <a href="#skill" className="text-base font-medium text-blue1 hover:text-gray-900">
                 Skill
               </a>
-              <a href="#portofolio" className="text-base font-medium text-white hover:text-gray-900">
+              <a href="#portofolio" className="text-base font-medium text-blue1 hover:text-gray-900">
                 Portofolio
               </a>
-              <a href="#contact" className="text-base font-medium text-white hover:text-gray-900">
+              <a href="#contact" className="text-base font-medium text-blue1 hover:text-gray-900">
                 Kontak Saya
               </a>
             </nav>
