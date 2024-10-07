bdfa64bcebaab6ce8c82eb8abcc7f61600d7aa7c
msdzilii <83410300+msdzilii@users.noreply.github.com>
2024-10-07 10:59:29 +0700

    edit style warna

--- src/components/nav/nav.js
+++ src/components/nav/nav.js
@@ -4,21 +4,21 @@ import { Popover, PopoverButton, PopoverPanel } from '@headlessui/react';
 
 const Header = () => {
   return (
-    <header className="fixed top-0 left-0 w-full bg-white shadow">
+    <header className="nav-section fixed top-0 left-0 w-full">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex justify-end items-center py-6">
           <div className="flex items-center">
             <nav className="hidden md:flex space-x-10 ml-10">
-              <a href="#aboutme" className="text-base font-medium text-blue-900 hover:text-blue-950">
+              <a href="#aboutme" className="text-base font-medium">
                 About Me
               </a>
-              <a href="#experience" className="text-base font-medium text-blue-900 hover:text-blue-950">
+              <a href="#experience" className="text-base font-medium">
                 Experience
               </a>
-              <a href="#portofolio" className="text-base font-medium text-blue-900 hover:text-blue-950">
+              <a href="#portofolio" className="text-base font-medium">
                 Portofolio
               </a>
-              <a href="#contact" className="text-base font-medium text-blue-900 hover:text-blue-950">
+              <a href="#contact" className="text-base font-medium">
                 Contact
               </a>
             </nav>
